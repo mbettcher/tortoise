45009b33d01f505aa61ba1ae66d2ddf665f6e5ae
mbettcher <mtonon.pmg@gmail.com>
2023-09-19 16:53:55 -0300

    first commit

---
+++ src/main/java/br/com/mtonon/tortoise/TortoiseApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.tortoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortoiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TortoiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mtonon/tortoise/TortoiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.tortoise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortoiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
